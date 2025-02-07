efda59f06b56ea20a4145f671ce25d6add2a107e
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2025-02-07 23:53:42 +0530

    changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
+import { fileURLToPath } from "url";
+
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,8 +16,8 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(cookieParser());
 app.use(
