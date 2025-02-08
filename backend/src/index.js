5b0b014aae89ffa96e8179f5bfba02df6eaf6a1f
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2025-02-08 08:37:57 +0530

    rootdir changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,6 +18,8 @@ const PORT = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__dirname);
+const rootDir = path.resolve(__dirname, "../../");
+console.log("Root Directory:", rootDir);
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -31,10 +33,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./frontend/dist")));
-
+  app.use(express.static(path.join(rootDir, "frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./frontend/dist", "index.html"));
+    res.sendFile(path.join(rootDir, "frontend/dist", "index.html"));
   });
 }
 
