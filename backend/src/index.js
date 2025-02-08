d5da6ac737b6c774450256db481de84e2e3e3bf7
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2025-02-08 08:31:39 +0530

    file dir changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,6 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 
-
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -18,6 +17,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__dirname);
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -31,10 +31,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "./frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "./frontend/dist", "index.html"));
   });
 }
 
